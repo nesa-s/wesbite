f70f793eb68fb016ccd161f09aecaeca3dd86032
nesa-s <nesas@umich.edu>
2025-06-03 15:14:03 -0400

    react-icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { BackgroundPaths } from "@/components/ui/background-paths";
+import { FaGithub, FaLinkedin, FaSpotify } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -15,6 +16,17 @@ export default function Home() {
           </>
         }
       />
+      <div className="absolute top-6 right-6 flex gap-4 text-2xl text-slate-700 dark:text-slate-200">
+  <a href="https://github.com/nesa-s" target="_blank" rel="noopener noreferrer" className="hover:text-black dark:hover:text-white">
+    <FaGithub />
+  </a>
+  <a href="https://linkedin.com/in/nesashamdasani/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600 dark:hover:text-blue-400">
+    <FaLinkedin />
+  </a>
+  <a href="https://open.spotify.com/user/by4hto07hdn817vgfbu1xahxv?si=5c20729922b14fd2" target="_blank" rel="noopener noreferrer" className="hover:text-green-500">
+    <FaSpotify />
+  </a>
+</div>
     </div>
   );
 }
